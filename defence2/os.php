b572a83dc745b2d77daad85afa011ad9c7473cf6
masaomi346 <masaomi346@gmail.com>
2018-03-16 16:30:45 +0900

    OSコマンドインジェクション

---
+++ defence2/os.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . "/function.php";
+login_session();
+header("Content-Type: text/html; charset=UTF-8");
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TEST</title>
+  </head>
+  <body>
+    <?php
+    $os = $_GET['os'];
+    $file = fopen("type $os","r");
+    while (($t = fgets($file)) !== FALSE){
+      print $t."<br />";
+    }
+    fclose($file);
+    ?>
+    <a href="main.php">戻る</a>
+  </body>
+</html>
